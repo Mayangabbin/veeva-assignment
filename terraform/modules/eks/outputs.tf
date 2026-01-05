48fe6327348b7fdcc31e62ebf6daf63de55e6f33
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-05 13:30:12 +0200

    Add kubeconfig output for EKS module

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -5,3 +5,11 @@ output "eks_cluster_name" {value = aws_eks_cluster.main.name}
 output "eks_node_role_arn" {value = aws_iam_role.eks_node_role.arn}
 
 output "eks_cluster_role_arn" {value = aws_iam_role.eks_cluster_role.arn}
+
+output "kubeconfig" {
+  value = {
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = aws_eks_cluster.main.certificate_authority[0].data
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
+}

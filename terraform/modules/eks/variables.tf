ff5d61a933a77b10cb64786ba66c819632414b86
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-05 12:22:30 +0200

    Rename variable 'enviroment' to 'environment'

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -9,5 +9,5 @@ variable "node_min_size" { type = number, default = 2 }
 variable "node_max_size" { type = number, default = 5 }
 variable "region" { type = string, default = "us-east-1" }
 variable "alb_iam_policy_file" { type = string, default = "alb_iam_policy.json" }
-variable "enviroment" { type = string }
+variable "environment" { type = string }
 variable "prefix" { type = string }

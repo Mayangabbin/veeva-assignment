c689d5eac842464b14186e3d7b7a53da8e55047f
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-05 10:19:19 +0200

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -55,10 +55,15 @@ resource "aws_eks_cluster" "main" {
   version  = "1.29"
 
   vpc_config {
-    subnet_ids = [for s in aws_subnet.private_app : s.id]  # משתמשים ב-private app subnets
+    subnet_ids = concat(
+      [for s in aws_subnet.private_app : s.id],
+      [for s in aws_subnet.public : s.id]
+    )
+    endpoint_public_access  = true
+    endpoint_private_access = true
   }
 
- ############ tags = {
+  tags = {
     Environment = "dev"
   }
 }

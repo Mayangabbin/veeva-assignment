945b14869eab5ccab1fe420060183687f94d1b3d
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-05 14:59:08 +0200

    Update main.tf

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -40,7 +40,7 @@ resource "aws_db_instance" "this" {
   allocated_storage       = var.allocated_storage
   name                    = var.db_name
   username                = var.db_username
-  password                = var.db_password
+  manage_master_user_password = true
   db_subnet_group_name    = aws_db_subnet_group.this.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   multi_az                = true

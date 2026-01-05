1aaa92c3bdd12a6bcabffc4931a9b31edb1c93e6
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-05 18:25:05 +0200

    Update providors.tf

--- terraform/providors.tf
+++ terraform/providors.tf
@@ -2,3 +2,9 @@
 provider "aws" {
   region = "eu-west-1"
 }
+# Provider for EKS cluster
+provider "kubernetes" {
+  host                   = module.eks.kubeconfig.host
+  cluster_ca_certificate = base64decode(module.eks.kubeconfig.cluster_ca_certificate)
+  token                  = module.eks.kubeconfig.token
+}

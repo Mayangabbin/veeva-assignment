2cbea67a883a26db5eb57e3ee1a180ee88391a4f
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-05 17:50:53 +0200

    Update main.tf

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -33,20 +33,21 @@ resource "aws_db_subnet_group" "this" {
 
 # RDS instance
 resource "aws_db_instance" "this" {
-  identifier              = var.db_name
-  engine                  = var.engine
-  engine_version          = var.engine_version
-  instance_class          = var.db_instance_class
-  allocated_storage       = var.allocated_storage
-  name                    = var.db_name
-  username                = var.db_username
+  identifier                  = var.db_name
+  engine                      = var.engine
+  engine_version              = var.engine_version
+  instance_class              = var.db_instance_class
+  allocated_storage           = var.allocated_storage
+  name                        = var.db_name
+  username                    = var.db_username
   manage_master_user_password = true
-  db_subnet_group_name    = aws_db_subnet_group.this.name
-  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
-  multi_az                = true
-  publicly_accessible     = false
-  skip_final_snapshot     = true
-  deletion_protection     = false
+  iam_database_authentication_enabled = true
+  db_subnet_group_name        = aws_db_subnet_group.this.name
+  vpc_security_group_ids      = [aws_security_group.rds_sg.id]
+  multi_az                    = true
+  publicly_accessible         = false
+  skip_final_snapshot         = true
+  deletion_protection         = false
 
   tags = {
     Name = var.db_name

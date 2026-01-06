24938bf75e0044c505ae8224412dd2e06c59c722
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-06 16:48:01 +0200

    Update variables.tf

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -96,6 +96,19 @@ variable "db_instance_class" {
   default = "db.t3.medium"
 }
 
+variable "db_engine" {
+  type    = string
+}
+
+variable "db_engine_version" {
+  type    = string
+}
+
+variable "db_allocated_storage" {
+  type    = number
+  default = 20
+}
+
 ## EKS nodes
 variable "node_desired_size" {
   type        = number

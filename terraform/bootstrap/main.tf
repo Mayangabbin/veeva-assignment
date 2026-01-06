256b1ae186e6b55aec35536099e499805d5746ce
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-06 17:29:36 +0200

    Create main.tf

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = var.region
+}
+
+# Create S3 bucket
+resource "aws_s3_bucket" "tf_state" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# Block public access
+resource "aws_s3_bucket_public_access_block" "tf_state" {
+  bucket                  = aws_s3_bucket.tf_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# DynamoDB table for state locking
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "${var.bucket_name}-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}

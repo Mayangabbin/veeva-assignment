2f464deb0a5619799436708a4605f593556bf6a7
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-06 16:47:53 +0200

    Update variables.tf

--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -6,10 +6,6 @@ variable "subnet_ids" {
   type = list(string)
 }
 
-variable "vpc_security_group_ids" {
-  type = list(string)
-}
-
 variable "nodes_sg_ids" {
   type = list(string)
 }
@@ -22,29 +18,20 @@ variable "db_username" {
   type = string
 }
 
-variable "db_password" {
-  type      = string
-  sensitive = true
-}
-
 variable "engine" {
   type    = string
-  default = "postgres"
 }
 
 variable "engine_version" {
   type    = string
-  default = "15.3"
 }
 
 variable "db_instance_class" {
   type    = string
-  default = "db.t3.medium"
 }
 
 variable "allocated_storage" {
   type    = number
-  default = 20
 }
 
 variable "tags" {

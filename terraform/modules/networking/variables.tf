c096b0ef7f6b07e77f3277eb32cf809323adeef1
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-06 14:55:41 +0200

    Update variables.tf

--- terraform/modules/networking/variables.tf
+++ terraform/modules/networking/variables.tf
@@ -1,9 +1,28 @@
-variable "cidr_block" { type = string }
+variable "cidr_block" {
+  type = string
+}
 
-variable "public_subnets" { type = map(string) }
-variable "private_app_subnets" { type = map(string) }
-variable "private_db_subnets" { type = map(string) }   
+variable "public_subnets" {
+  type = map(string)
+}
 
-variable "prefix" { type = string }
-variable "cluster_name" { type = string }
+variable "private_app_subnets" {
+  type = map(string)
+}
 
+variable "private_db_subnets" {
+  type = map(string)
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}

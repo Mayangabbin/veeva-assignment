87ccbb78a403eb4b35b0ed5cb250647aca11ee42
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-06 15:00:17 +0200

    Update variables.tf

--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -1,39 +1,53 @@
-variable "vpc_security_group_ids" {type = list(string)}
-
-variable "subnet_ids" {type = list(string)}
-
-variable "nodes_sg_ids" {type = list(string)}
-
-variable "vpc_id" {type = string}
-
-variable "db_name" {type = string}
-
-variable "db_username" {type = string}
-
-variable "db_password" {
-  type        = string
-  sensitive   = true
+variable "vpc_id" {
+  type = string
 }
 
-variable "db_instance_class" {
-  type        = string
-  default     = "db.t3.medium"
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "vpc_security_group_ids" {
+  type = list(string)
+}
+
+variable "nodes_sg_ids" {
+  type = list(string)
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
 }
 
 variable "engine" {
-  type        = string
-  default     = "postgres"
+  type    = string
+  default = "postgres"
 }
 
 variable "engine_version" {
-  type        = string
-  default     = "15.3"
+  type    = string
+  default = "15.3"
+}
+
+variable "db_instance_class" {
+  type    = string
+  default = "db.t3.medium"
 }
 
 variable "allocated_storage" {
-  type        = number
-  default     = 20
+  type    = number
+  default = 20
 }
 
-
-
+variable "tags" {
+  type    = map(string)
+  default = {}
+}

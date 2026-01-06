207a64dd88fe9430cc0dad5a2c38257ed699e32f
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-06 12:53:36 +0200

    Create main.tf

---
+++ terraform/modules/cloudfront/main.tf
@@ -0,0 +1,40 @@
+# Get frontend ALB
+data "aws_lb" "frontend_alb" {
+  filter {
+    name   = "tag:kubernetes.io/cluster/${var.cluster_name}"
+    values = ["owned"]
+  }
+
+  filter {
+    name   = "tag:kubernetes.io/ingress-name"
+    values = [var.ingress_name]
+  }
+}
+
+# CloudFront distribution
+resource "aws_cloudfront_distribution" "frontend_cf" {
+  origin {
+    domain_name = data.aws_lb.frontend_alb.dns_name
+    origin_id   = "frontend-alb-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "frontend-alb-origin"
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}

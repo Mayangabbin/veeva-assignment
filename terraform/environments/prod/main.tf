de8188ca3c9612e5526a8091bdab0c382c5ec92c
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-06 16:47:56 +0200

    Update main.tf

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -55,6 +55,7 @@ module "rds" {
   db_username       = var.db_username
   db_instance_class = var.db_instance_class
   engine            = var.db_engine
+  allocated_storage = db_allocated_storage
   node_sg_ids       = [module.eks.eks_node_group_sg_id]
   subnet_ids        = [for az, cidr in module.networking.private_app_subnets : cidr]
 }

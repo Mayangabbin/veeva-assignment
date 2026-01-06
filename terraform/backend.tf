4a0d6ee4bd48c18420060562226a5712755cfaf0
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-06 14:12:38 +0200

    Create backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket"
+    key            = "envs/prod/terraform.tfstate" 
+    region         = "eu-central-1"
+    encrypt        = true
+  }
+}

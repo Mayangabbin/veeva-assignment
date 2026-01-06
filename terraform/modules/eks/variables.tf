bf794b5a949da195c1b6cdac3f8b6e9bf4093bfc
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-06 14:44:24 +0200

    Update variables.tf

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,14 +1,58 @@
-variable "cluster_name" { type = string }
-variable "region" { type = string }
-variable "eks_version" { type = string, default = "1.29" }
-variable "vpc_id" { type = string }
-variable "public_subnet_ids" { type = list(string) }
-variable "private_subnet_ids" { type = list(string) }
-variable "node_instance_type" { type = string, default = "t3.medium" }
-variable "node_desired_size" { type = number, default = 2 }
-variable "node_min_size" { type = number, default = 2 }
-variable "node_max_size" { type = number, default = 5 }
-variable "region" { type = string, default = "us-east-1" }
-variable "alb_iam_policy_file" { type = string, default = "alb_iam_policy.json" }
-variable "environment" { type = string }
-variable "prefix" { type = string }
+variable "cluster_name" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+}
+
+variable "eks_version" {
+  type    = string
+  default = "1.29"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "t3.medium"
+}
+
+variable "node_desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_min_size" {
+  type    = number
+  default = 2
+}
+
+variable "node_max_size" {
+  type    = number
+  default = 5
+}
+
+variable "alb_iam_policy_file" {
+  type    = string
+  default = "alb_iam_policy.json"
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}

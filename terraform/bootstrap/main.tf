3d271222039455818c3a5add1e8dc92c51694b2b
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-06 17:33:33 +0200

    Update main.tf

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -31,7 +31,7 @@ resource "aws_s3_bucket_public_access_block" "tf_state" {
 
 # DynamoDB table for state locking
 resource "aws_dynamodb_table" "tf_lock" {
-  name         = var.table_name
+  name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 

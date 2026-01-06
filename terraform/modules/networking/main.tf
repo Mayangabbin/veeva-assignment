ce461b1e2cc506a08eded1d5df876ad4d2bfdcd6
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-06 14:54:33 +0200

    Update main.tf

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -13,9 +13,12 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "${var.prefix}-igw"
-  }
+  tags = merge(
+    {
+      Name = "${var.prefix}-vpc"
+    },
+    var.tags
+  )
 }
 
 # Public subnets
@@ -27,12 +30,14 @@ resource "aws_subnet" "public" {
   availability_zone       = each.key
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "${var.prefix}-public-${each.key}"
-    kubernetes.io/role/elb = 1
-    kubernetes.io/cluster/${var.cluster_name} = shared
-
-  }
+  tags = merge(
+    {
+      Name = "${var.prefix}-public-${each.key}"
+      "kubernetes.io/role/elb" = 1
+      "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    },
+    var.tags
+  )
 }
 
 # Route Table for Public Subnets
@@ -44,9 +49,12 @@ resource "aws_route_table" "public" {
     gateway_id = aws_internet_gateway.main.id
   }
 
-  tags = {
-    Name = "${var.prefix}-public-rt"
-  }
+  tags = merge(
+    {
+      Name = "${var.prefix}-public-rt"
+    },
+    var.tags
+  )
 }
 
 # Associate Public Subnets with Public Route Table
@@ -62,9 +70,12 @@ resource "aws_eip" "nat" {
   for_each = var.public_subnets
   vpc = true
 
-  tags = {
-    Name = "${var.prefix}-nat-eip-${each.key}"
-  }
+  tags = merge(
+    {
+      Name = "${var.prefix}-nat-eip-${each.key}"
+    },
+    var.tags
+  )
 }
 
 # NAT Gateway in public subnets
@@ -74,9 +85,12 @@ resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat[each.key].id
   subnet_id     = aws_subnet.public[each.key].id
 
-  tags = {
-    Name = "${var.prefix}-nat-gateway-${each.key}"
-  }
+  tags = merge(
+    {
+      Name = "${var.prefix}-nat-gateway-${each.key}"
+    },
+    var.tags
+  )
 }
 
 # Private app subnets
@@ -87,12 +101,15 @@ resource "aws_subnet" "private_app" {
   cidr_block        = each.value
   availability_zone = each.key
 
-  tags = {
-    Name = "${var.prefix}-private-app-${each.key}"
-    Tier = "application"
-    kubernetes.io/role/internal-elb = 1
-    kubernetes.io/cluster/${var.cluster_name}- = "shared"
-  }
+  tags = merge(
+    {
+      Name = "${var.prefix}-private-app-${each.key}"
+      Tier = "application"
+      "kubernetes.io/role/internal-elb" = 1
+      "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    },
+    var.tags
+  )
 }
 
 # App private route tables
@@ -100,9 +117,12 @@ resource "aws_route_table" "private_app" {
   for_each = var.private_app_subnets
   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "${var.prefix}-private-app-rt-${each.key}"
-  }
+  tags = merge(
+    {
+      Name = "${var.prefix}-private-app-rt-${each.key}"
+    },
+    var.tags
+  )
 }
 
 # Routes to NAT Gateways
@@ -130,19 +150,25 @@ resource "aws_subnet" "private_db" {
   cidr_block        = each.value
   availability_zone = each.key
 
-  tags = {
-    Name = "${var.prefix}-private-db-${each.key}"
-    Tier = "database"
-  }
+  tags = merge(
+    {
+      Name = "${var.prefix}-private-db-${each.key}"
+      Tier = "database"
+    },
+    var.tags
+  )
 }
 
 # DB private route table
 resource "aws_route_table" "private_db" {
   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "${var.prefix}-private-db-route-table"
-  }
+  tags = merge(
+    {
+      Name = "${var.prefix}-private-db-route-table"
+    },
+    var.tags
+  )
 }
 
 resource "aws_route_table_association" "private_db" {

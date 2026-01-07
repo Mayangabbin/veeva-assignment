bbc8d787f387b20a989f8ec97e35fbca93561ad4
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-07 09:35:38 +0200

    Update providors.tf

--- terraform/environments/prod/providors.tf
+++ terraform/environments/prod/providors.tf
@@ -4,6 +4,7 @@ provider "aws" {
 }
 # Provider for EKS cluster
 provider "kubernetes" {
+  depends_on = [module.eks]
   host                   = module.eks.kubeconfig.host
   cluster_ca_certificate = base64decode(module.eks.kubeconfig.cluster_ca_certificate)
   token                  = module.eks.kubeconfig.token

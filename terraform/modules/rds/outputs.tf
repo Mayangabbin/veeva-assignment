1622ee4a0e952be623d4361cad2354c3d8bf4dd2
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-07 09:23:15 +0200

    Update outputs.tf

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,11 +1,6 @@
 output "endpoint" {
   value = aws_db_instance.this.endpoint
 }
-
-output "port" {
-  value = aws_db_instance.this.port
-}
-
 output "db_instance_id" {
   value = aws_db_instance.this.id
 }

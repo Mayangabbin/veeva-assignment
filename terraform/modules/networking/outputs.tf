35856bb62fe6411094cee9efc42ce885269dc04d
Mayangabbin <99660952+Mayangabbin@users.noreply.github.com>
2026-01-07 09:19:29 +0200

    Update outputs.tf

--- terraform/modules/networking/outputs.tf
+++ terraform/modules/networking/outputs.tf
@@ -1,5 +1,21 @@
-output "vpc_id" { value = aws_vpc.main.id }
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
 
-output "public_subnet_ids" { value = [for s in aws_subnet.public : s.id] }
-output "private_app_subnet_ids" { value = [for s in aws_subnet.private_app : s.id] }
-output "private_db_subnet_ids" { value = [for s in aws_subnet.private_db : s.id] }
+output "public_subnet_ids" {
+  value = [
+    for s in aws_subnet.public : s.id
+  ]
+}
+
+output "private_app_subnet_ids" {
+  value = [
+    for s in aws_subnet.private_app : s.id
+  ]
+}
+
+output "private_db_subnet_ids" {
+  value = [
+    for s in aws_subnet.private_db : s.id
+  ]
+}
